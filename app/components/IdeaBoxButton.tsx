5baf325a7d2863b42d7c57df826a0cdf29865756
feliarroyo <farroyo@alumnos.exa.unicen.edu.ar>
2026-09-11 11:03:55 -0300

    fix: idea button route corrected

--- app/components/IdeaBoxButton.tsx
+++ app/components/IdeaBoxButton.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function IdeaBoxButton() {
     return (
-        <a href="/projects" target="_blank" rel="noopener noreferrer">
+        <a href="/ideas" target="_blank" rel="noopener noreferrer">
             <Image
                 src="/images/icons/house/house.png"
                 alt="Future Ideas"
