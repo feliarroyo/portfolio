454129912a3ccd5c226b0bcb9f21c4fca3d73a25
feliarroyo <farroyo@alumnos.exa.unicen.edu.ar>
2026-09-11 11:03:28 -0300

    chore: added placeholder avatar image

--- app/components/AboutMeButton.tsx
+++ app/components/AboutMeButton.tsx
@@ -1,3 +1,16 @@
-export default function AboutMeButton(){
-    return(<button>About Me</button>)
+import Image from "next/image";
+
+export default function AboutMeButton() {
+    return (
+        <a href="/about-me" target="_blank" rel="noopener noreferrer">
+            <Image
+                src="/images/icons/avatar/avatar.png"
+                alt="About Me"
+                width={32}
+                height={32}
+                unoptimized
+                style={{ imageRendering: 'pixelated' }}
+            />
+        </a>
+    )
 }
