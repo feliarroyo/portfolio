54cd685504889643089075d3ad0be94d0bc35901
feliarroyo <farroyo@alumnos.exa.unicen.edu.ar>
2026-09-11 14:44:11 -0300

    chore: Setup locales for future language toggle

---
+++ app/locales/es.ts
@@ -0,0 +1,16 @@
+export const es = {
+  header: {
+    toggleTheme: "Alternar modo oscuro",
+    language: "Cambiar idioma",
+    github: "Perfil de GitHub",
+    linkedin: "Perfil de LinkedIn",
+  },
+  home: {
+    title: "Felipe Arroyo",
+    aboutMe: "Sobre mí",
+    projects: "Proyectos",
+    ideaBox: "Ideas futuras",
+  },
+};
+
+export type Translations = typeof es;
