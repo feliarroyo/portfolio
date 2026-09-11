d9eb690eed604ed5e8ee44ca129eca0b4d44fbed
feliarroyo <farroyo@alumnos.exa.unicen.edu.ar>
2026-09-10 23:16:24 -0300

    feat: made project button scaling pixelated

--- app/components/ProjectsButton.tsx
+++ app/components/ProjectsButton.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function ProjectsButton() {
     return (
         <a href="/projects" target="_blank" rel="noopener noreferrer">
-            <Image src="/images/icons/chest/chest_3.png" alt="Projects" width={24} height={24} />
+            <Image 
+                src="/images/icons/chest/chest_3.png" 
+                alt="Projects" 
+                width={32} 
+                height={32}
+                unoptimized
+                style = {{imageRendering: 'pixelated'}}
+            />
         </a>
     )
 }
