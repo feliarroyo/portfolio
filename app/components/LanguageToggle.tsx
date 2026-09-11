f9375ac4281d419d2d1ac266dad763c49c5e222f
feliarroyo <farroyo@alumnos.exa.unicen.edu.ar>
2026-09-11 11:25:34 -0300

    feat: added language toggle image

--- app/components/LanguageToggle.tsx
+++ app/components/LanguageToggle.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
+
 interface LanguageToggleProps {
   className?: string;
 }
 
 export default function LanguageToggle({ className }: LanguageToggleProps) {
   return (
-    <div className={`${className}`}>
-      <p>Language Toggle</p>
-    </div>
+    <a href="/language" target="_blank" rel="noopener noreferrer">
+      <Image
+        src="/images/icons/lang_toggle.png"
+        alt="Language Toggle"
+        width={54}
+        height={54}
+        unoptimized
+        style={{ imageRendering: 'pixelated' }}
+      />
+    </a>
   );
 }
