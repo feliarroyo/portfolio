7fa8377ead5a271a83f6a339c85a023deab6ef54
feliarroyo <farroyo@alumnos.exa.unicen.edu.ar>
2026-09-11 10:24:08 -0300

    feat: added future ideas icon

--- app/components/IdeaBoxButton.tsx
+++ app/components/IdeaBoxButton.tsx
@@ -1,3 +1,16 @@
-export default function IdeaBoxButton(){
-    return(<button>Idea Box</button>)
+import Image from "next/image";
+
+export default function IdeaBoxButton() {
+    return (
+        <a href="/projects" target="_blank" rel="noopener noreferrer">
+            <Image
+                src="/images/icons/house/house.png"
+                alt="Future Ideas"
+                width={32}
+                height={32}
+                unoptimized
+                style={{ imageRendering: 'pixelated' }}
+            />
+        </a>
+    )
 }
